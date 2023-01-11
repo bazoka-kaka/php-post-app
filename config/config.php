65c7fb710e1eb5562cb03c725e7c82fa8eac0d2c
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-01-11 17:28:45 +0700

    add config.php

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+  define("ROOT_URL", "http://localhost:3500/");
+  define("DB_HOST", "127.0.0.1");
+  define("DB_USER", "benzion");
+  define("DB_PASS", "@Benzion123");
+  define("DB_NAME", "phppost");
